acb6c27af063da9fbd8809141636c7886005ed20
TP054682 <TP054682@DESKTOP-7J491RR>
2021-05-14 22:43:19 +0800

    OODJ_Assignment
    - Changed Login Layout Design

--- src/Assignment/Assignment.java
+++ src/Assignment/Assignment.java
@@ -1,5 +1,4 @@
 package Assignment;
-import java.awt.Label;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
--- src/Assignment/JFrameFormat.java
+++ src/Assignment/JFrameFormat.java
@@ -10,7 +10,6 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
