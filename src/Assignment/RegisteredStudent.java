32b8e369c67ad36d5ba61e69310b86afd23481be
Acer <Acer@LAPTOP-DKODMA4K>
2021-05-15 15:06:31 +0800

    New Release ----------- Add changes in RegisteredStudent

--- src/Assignment/RegisteredStudent.java
+++ src/Assignment/RegisteredStudent.java
@@ -7,24 +7,28 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class RegisteredStudent{    
     private JPanel header, menuBody, displayBody, sidePanel;
     private String user, time;
-    private Button viewCoach, viewRecord, viewRegSport, modifyRecord, feedbackCoach, payment, attendance, logout; 
+    private JButton viewCoach, viewRecord, viewRegSport, modifyRecord, feedbackCoach, payment, attendance, logout; 
+    private ImageIcon viewCoachLogo, viewRecordLogo, viewRegSportLogo;
     public void FrameLoad(String UserName){
         //++Logout Button++//
         JFrameFormat frame = new JFrameFormat(UserName);
         frame.setVisible(true);
         
-        logout = new Button("Logout");
+        logout = new JButton("Logout");
         logout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
@@ -32,55 +36,61 @@ public class RegisteredStudent{
                 Assignment.login.setVisible(true);
             }
         });
-        viewCoach = new Button("View Coach");
+        viewCoach = new JButton("View Coach");
         viewCoach.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        viewRecord = new Button("View Self Records");
+        viewRecord = new JButton("View Self Records");
         viewRecord.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        viewRegSport = new Button("View Sport Schedule");
+        viewRegSport = new JButton("View Sport Schedule");
         viewRegSport.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        modifyRecord = new Button("Modify Self Records");
+        modifyRecord = new JButton("Modify Self Records");
         modifyRecord.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        feedbackCoach = new Button("Feedback");
+        feedbackCoach = new JButton("Feedback");
         feedbackCoach.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        attendance = new Button("Attendance");
+        attendance = new JButton("Attendance");
         attendance.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
-        payment = new Button("Payment");
+        payment = new JButton("Payment");
         payment.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 
             }
         });
+        viewCoachLogo = new ImageIcon(new ImageIcon(this.getClass().getResource("/PicLibrary/staff.png")).getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
+        viewCoach = new JButton("View Coach", viewCoachLogo);
+        viewRecordLogo = new ImageIcon(new ImageIcon(this.getClass().getResource("/PicLibrary/record.png")).getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
+        viewRecord = new JButton("View Self Record", viewRecordLogo);
+        viewRegSportLogo = new ImageIcon(new ImageIcon(this.getClass().getResource("/PicLibrary/schedule.png")).getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
+        viewRegSport = new JButton("View Sport Schedule", viewRegSportLogo);
         DateFormat dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
         String strDate = dateFormat.format(Assignment.DateTime);
         JLabel dateTime = new JLabel("Login DateTime: "+strDate, JLabel.CENTER);
